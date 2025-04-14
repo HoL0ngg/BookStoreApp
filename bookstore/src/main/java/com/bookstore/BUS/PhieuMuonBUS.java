57917320a133a2c8ab038ca0f2ce3f0f24e3aee4
KongDanh <danhcong6205@gmail.com>
2025-04-15 00:50:56 +0700

    them phieu muon (chua hoan thanh)

---
+++ bookstore/src/main/java/com/bookstore/BUS/PhieuMuonBUS.java
@@ -0,0 +1,104 @@
+package com.bookstore.BUS;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.crypto.Data;
+
+import com.bookstore.DTO.PhieuMuonDTO;
+import com.bookstore.utils.DatabaseUtils;
+import com.bookstore.dao.PhieuMuonDAO;;
+
+public class PhieuMuonBUS {
+
+    private List<PhieuMuonDTO> listPhieuMuon;
+
+    public PhieuMuonBUS(){
+        listPhieuMuon = new PhieuMuonDAO().layDanhSachPhieuMuon();
+    }
+
+    public PhieuMuonDTO timPhieuMuon(int MaPhieuMuon){
+        String sql = "SELECT * FROM PhieuMuon WHERE MaPhieuMuon = ?";
+        try (Connection conn = DatabaseUtils.getConnection();
+        PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setInt(1, MaPhieuMuon);
+            try (ResultSet rs = stmt.executeQuery()){
+                if (rs.next()){
+                    return new PhieuMuonDTO(
+                        rs.getInt("MaPhieuMuon"),
+                        rs.getDate("NgayMuon"),
+                        rs.getDate("NgayTraDuKien"),
+                        rs.getInt("TrangThai"),
+                        rs.getString("MaDocGia"),
+                        rs.getString("MaNhanVien")
+                    );
+                }
+            }
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    // thêm phiếu mượn
+    public boolean themPhieuMuon(PhieuMuonDTO phieuMuon){
+        String sql = "INSERT INTO PhieuMuon (MaPhieuMuon, NgayMuon, NgayTraDuKien, TrangThai, MaDocGia, MaNhanVien) VALUES (?,?,?,?,?,?)";
+        try (Connection conn = DatabaseUtils.getConnection();
+            PreparedStatement stmt = conn.prepareStatement(sql)){
+            stmt.setInt(1, phieuMuon.getMaPhieuMuon());
+            stmt.setDate(2, new java.sql.Date(phieuMuon.getNgayMuon().getTime()));
+            stmt.setDate(3, (Date) phieuMuon.getNgayTraDuKien());
+            stmt.setInt(4, phieuMuon.getTrangThai());
+            stmt.setString(5, phieuMuon.getMaDocGia());
+            stmt.setString(6, phieuMuon.getMaNhanVien());
+            
+            return stmt.executeUpdate() > 0;
+        } catch (SQLException e){
+            e.printStackTrace();
+
+            return false;
+        }
+    }
+    
+    // Sửa phiếu mượn
+    public boolean suaPhieuMuon(PhieuMuonDTO phieuMuon){
+        String sql = "UPDATE PhieuMuon SET NgayMuon = ?, NgayTraDuKien = ?, TrangThai = ?, MaDocGia = ?, MaNhanVien = ? WHERE MaPhieuMuon = ?";
+        try (Connection conn = DatabaseUtils.getConnection();
+            PreparedStatement stmt = conn.prepareStatement(sql)){
+                stmt.setDate(1, (Date) phieuMuon.getNgayMuon());
+                stmt.setDate(2, (Date) phieuMuon.getNgayTraDuKien());
+                stmt.setInt(3, phieuMuon.getTrangThai());
+                stmt.setString(4, phieuMuon.getMaDocGia());
+                stmt.setString(5, phieuMuon.getMaNhanVien());
+                
+                return stmt.executeUpdate() > 0;
+        } catch (SQLException e){
+            e.printStackTrace();
+            
+            return false;
+        }
+    }
+
+    // Xóa phiếu mượn
+    public boolean xoaPhieuMuon(int maPhieuMuon){
+        String sql = "DELETE DROM PhieuMuon WHERE MaPhieuMuon = ?";
+        try (Connection conn = DatabaseUtils.getConnection();
+            PreparedStatement stmt = conn.prepareStatement(sql)){
+                stmt.setInt(1, maPhieuMuon);
+
+                return stmt.executeUpdate() > 0;
+            } catch (SQLException e){
+                e.printStackTrace();
+
+                return false;
+            }
+    }
+
+    public void themChiTietPhieuMuon(int mpm, String ms){}
+
+}
---
+++ bookstore/src/main/java/com/bookstore/DTO/PhieuMuonDTO.java
@@ -0,0 +1,79 @@
+package com.bookstore.DTO;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class PhieuMuonDTO {
+    
+    private int MaPhieuMuon;
+    private Date NgayMuon;
+    private Date NgayTraDuKien;
+    private int TrangThai;
+    private String MaDocGia;
+    private String MaNhanVien;
+
+    public PhieuMuonDTO(int MaPhieuMuon, Date NgayMuon, Date NgayTraDuKien, int TrangThai,
+                    String MaDocGia, String MaNhanVien){
+        this.MaPhieuMuon = MaPhieuMuon;
+        this.NgayMuon = NgayMuon;
+        this.NgayTraDuKien = NgayTraDuKien;
+        this.TrangThai = TrangThai;
+        this.MaDocGia = MaDocGia;
+        this.MaNhanVien = MaNhanVien;
+
+    }
+
+    public int getMaPhieuMuon() {
+        return MaPhieuMuon;
+    }
+
+    public void setMaPhieuMuon(int maPhieuMuon) {
+        MaPhieuMuon = maPhieuMuon;
+    }
+
+    public Date getNgayMuon() {
+        return NgayMuon;
+    }
+
+    public void setNgayMuon(Date ngayMuon) {
+        NgayMuon = ngayMuon;
+        setNgayTraDuKien(ngayMuon);
+    }
+
+    public Date getNgayTraDuKien() {
+        return NgayTraDuKien;
+    }
+
+    private void setNgayTraDuKien(Date ngayMuon) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(ngayMuon);
+        cal.add(Calendar.DAY_OF_MONTH, 15);
+        this.NgayTraDuKien = cal.getTime(); 
+    }
+
+    public int getTrangThai() {
+        return TrangThai;
+    }
+
+    public void setTrangThai(int trangThai) {
+        TrangThai = trangThai;
+    }
+
+    public String getMaDocGia() {
+        return MaDocGia;
+    }
+
+    public void setMaDocGia(String maDocGia) {
+        MaDocGia = maDocGia;
+    }
+
+    public String getMaNhanVien() {
+        return MaNhanVien;
+    }
+
+    public void setMaNhanVien(String maNhanVien) {
+        MaNhanVien = maNhanVien;
+    }
+
+    
+}
